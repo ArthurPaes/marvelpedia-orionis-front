92fdceb104a8b90e815bde09cc264c1f87922f34
Arthur <tercmt-arthurs@ftd.com.br>
2023-10-18 13:42:20 -0300

    chore: adicionando estrutura de pastas simples

---
+++ src/app/pages/users/users.module.ts
@@ -0,0 +1,20 @@
+// @angular
+import { NgModule } from '@angular/core';
+
+
+
+// Components
+import { UsersComponent } from './users.component';
+
+
+@NgModule({
+	imports: [
+	],
+	declarations: [
+        UsersComponent,
+    ],
+    providers: [
+  
+    ]
+})
+export class UsersModule { }
