75e2ec4e005d5ef63e23ed7fefb4de4832fa2b8a
unknown <brennerpaiva@gmail.com>
2023-10-20 13:55:20 -0300

    feat: criando lógica para armazenar valor do checkbox e exportar esse valor para o componente pai

--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
@@ -7,4 +7,13 @@ import { Component, Input } from '@angular/core';
 })
 export class CheckboxComponent {
   @Input() label: string = '';
+  checked: boolean = false;
+  @Output() checkedChange = new EventEmitter<boolean>();
+
+  onCheckboxChange(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    this.checked = checkbox.checked;
+    console.log(this.checked);
+    this.checkedChange.emit(this.checked); // Emite o valor para o componente pai
+  }
 }
