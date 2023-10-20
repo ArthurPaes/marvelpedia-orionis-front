3edf715105d273e8302b7234c8578c6e9fa16e40
unknown <brennerpaiva@gmail.com>
2023-10-20 13:44:53 -0300

    feat: adicionado prop label ao componente checkbox

--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss'],
 })
-export class CheckboxComponent {}
+export class CheckboxComponent {
+  @Input() label: string = '';
+}
