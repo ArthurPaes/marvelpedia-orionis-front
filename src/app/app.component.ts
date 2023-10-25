8de3d50f239340302385db3fb56efd1381c1934e
rafael-horauti <rafael.horauti@uiclap.com>
2023-10-24 23:09:58 -0300

    correções de conflito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-<<<<<<< HEAD
-export class AppComponent {
-
-}
-=======
 export class AppComponent {}
->>>>>>> 49a0cadd3cf573220788a299e64ec8f4d9e10d3a
