5afced37fffd50e11670edb14ee0a6ccc430a86c
unknown <brennerpaiva@gmail.com>
2023-10-26 19:14:27 -0300

    feat: página de login criada e estilizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputComponent } from './components/input/input.component';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { LoginComponent } from './pages/login/login.component';
+import { FormLoginComponent } from './components/form-login/form-login.component';
+import { InputComponent } from './components/input/input.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CheckboxComponent } from 'src/app/components/checkbox/checkbox.component';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    FormLoginComponent,
+    InputComponent,
+    ButtonComponent,
+    CheckboxComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
