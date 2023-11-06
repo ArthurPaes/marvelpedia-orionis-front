b9ace93d4d3817a5774b452e0cbb1d439802ed85
unknown <brennerpaiva@gmail.com>
2023-11-06 09:49:26 -0300

    feat: adiciona ícones de favorito e notificação ao card

--- src/app/components/character-card/character-card.component.ts
+++ src/app/components/character-card/character-card.component.ts
@@ -9,6 +9,28 @@ export class CharacterCardComponent {
   @Input() title = '';
   @Input() description = '';
   @Input() imageSrc = '';
+  favorited = false;
+  notified = false;
+
+  /**
+   * toggleNotified()
+   *
+   * Responsável por alternar o valor da variável `notified` entre `true` e `false` toda vez que o usuário clica no ícone de notificação.
+   */
+  toggleNotified() {
+    this.notified = !this.notified;
+    console.log(this.notified);
+  }
+
+  /**
+   * toggleFavorited()
+   *
+   * Responsável por alternar o valor da variável `favorited` entre `true` e `false` toda vez que o usuário clica no ícone de favorito.
+   */
+  toggleFavorited() {
+    this.favorited = !this.favorited;
+    console.log(this.favorited);
+  }
 
   /**
    * showCharacterDetails()
