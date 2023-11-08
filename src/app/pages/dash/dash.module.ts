4832f62af836aae41aa5e12b6b63ecbc8f79b74e
unknown <brennerpaiva@gmail.com>
2023-11-08 15:43:37 -0300

    cria módulo para páginas de dashboard

--- src/app/pages/dash/dash.module.ts
+++ src/app/pages/dash/dash.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 
-import { LoginComponent } from './login/login.component';
-import { authApi } from 'src/app/core/api/app/auth.api';
+import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [HomeComponent],
   imports: [CommonModule, MatIconModule, FormsModule, ComponentsModule],
-  providers: [authApi],
+  providers: [],
   bootstrap: [],
-  exports: [LoginComponent],
+  exports: [HomeComponent],
 })
-export class AuthModule {}
+export class DashModule {}
