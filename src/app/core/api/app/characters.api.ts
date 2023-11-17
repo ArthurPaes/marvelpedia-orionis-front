5b13f4a364eecc33ca9e382ef69bf2df48bb7132
Brenner Paiva <brennerpaiva@gmail.com>
2023-11-17 01:53:50 -0300

    feat: criando e utilizando variável de ambiente na aplicação

--- src/app/core/api/app/characters.api.ts
+++ src/app/core/api/app/characters.api.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpRequestService } from '../http-request.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharactersApi {
+  private apiUrl = `${environment.API_BASE_URL}`;
+
   constructor(private httpGetCharacter: HttpRequestService) {}
 
   async getCharacters(page: number): Promise<any> {
     return await this.httpGetCharacter.sendHttpRequest(
-      `http://localhost:4444/v1/getPage/characters/${page}`,
+      `${this.apiUrl}/characters?page=${page}`,
       'GET',
     );
   }
