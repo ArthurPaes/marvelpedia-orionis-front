a6cc7609d5b91e327fca521b60ff6269cfc1e9a3
Brenner Paiva <brennerpaiva@gmail.com>
2023-11-22 17:30:35 -0300

    docs: implementa TsDoc ao getContentByCategorie

--- src/app/core/api/app/marvel-content.api.ts
+++ src/app/core/api/app/marvel-content.api.ts
@@ -12,6 +12,16 @@ export class MarvelContentApi {
 
   constructor(private httpRequestService: HttpRequestService) {}
 
+  /**
+   * getContentByCategorie
+   *
+   * Obtém o conteúdo da API com base na categoria especificada.
+   * A categoria pode ser uma das seguintes: 'characters', 'comics', 'series', 'stories', 'favorites', 'events'.
+   * @param category - A categoria do conteúdo desejado.
+   * @param page - O número da página a ser exibida com o conteudo (a cada página são exibido até 9 cards).
+   * @param search - Uma string de pesquisa opcional para filtrar os resultados.
+   * @returns retorna uma Promise contendo um array objetos contendo os conteúdos da categoria especificada.
+   */
   async getContentByCategorie(
     category: EnumContentCategory,
     page: number,
