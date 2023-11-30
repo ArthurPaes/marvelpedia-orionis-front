fc13ce69dd2ed01b6587ac564e0985e797950e9a
Rod <rodrigodip@gmail.com>
2023-11-30 19:31:39 -0300

    Feat(pages/othersDetails): Cria o componente.

---
+++ src/app/pages/dash/others-details/others-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-others-details',
+  templateUrl: './others-details.component.html',
+  styleUrls: ['./others-details.component.scss'],
+})
+export class OthersDetailsComponent {}
