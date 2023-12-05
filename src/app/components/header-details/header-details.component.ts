8ac608a35bffe4f08c8ef28da0022ba5aade1f09
Brenner Paiva <brennerpaiva@gmail.com>
2023-12-05 11:02:54 -0300

    feat: cria componente de header das páginas de detalhes

---
+++ src/app/components/header-details/header-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header-details',
+  templateUrl: './header-details.component.html',
+  styleUrls: ['./header-details.component.scss'],
+})
+export class HeaderDetailsComponent {
+  constructor(
+    private route: Router,
+    private location: Location,
+  ) {}
+
+  @Input() ContentName = '';
+  @Input() ContentDescription = '';
+  @Input() ContentThumb = '';
+  @Input() ContentUrlButton = '';
+
+  /**
+   * goBack
+   *
+   * Função que retorna uma página anterior ao que o usuário estava navegando.
+   */
+  goBack(): void {
+    console.log(this.location);
+    this.location.back();
+  }
+}
