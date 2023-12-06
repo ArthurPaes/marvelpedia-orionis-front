3924fda3c683cb4a3b77ccdc23ef91456805e4f7
rafael-horauti <rafael.horauti@uiclap.com>
2023-12-06 14:35:01 -0300

    alteração de nome customOptions

--- src/app/pages/dash/character-details/character-details.component.ts
+++ src/app/pages/dash/character-details/character-details.component.ts
@@ -64,7 +64,7 @@ export class CharacterDetailsComponent implements OnInit {
     this.getCategoryList();
   }
 
-  customOptions: OwlOptions = {
+  carouselOptions: OwlOptions = {
     loop: false,
     skip_validateItems: true,
     mouseDrag: true,
